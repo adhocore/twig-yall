c63e6bcebe949a498c9341475a1653537558ff42
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-12-14 20:04:44 +0700

    test: add stub parser test

---
+++ tests/ParserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the TWIG-YALL package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
+namespace Ahc\TwigYall\Test;
+
+use Ahc\TwigYall\Yall;
+use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+
+class ParserTest extends TestCase
+{
+
+}
